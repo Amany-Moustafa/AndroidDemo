f8c3b08647af4c050e74e8f76e5f81647217d306
Amany-Moustafa <amany.af@gmail.com>
2018-04-19 19:51:12 +0200

    using mockito

---
+++ app/src/androidTest/java/com/mytaxi/android_demo/test/LoginActivityTest.java
@@ -0,0 +1,61 @@
+package com.mytaxi.android_demo.test;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.mytaxi.android_demo.TestsApp;
+import com.mytaxi.android_demo.activities.MainActivity;
+import com.mytaxi.android_demo.data.TestData;
+import com.mytaxi.android_demo.pages.LoginPage;
+import com.mytaxi.android_demo.pages.SearchPage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import javax.inject.Inject;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class LoginActivityTest {
+    LoginPage loginObject;
+    SearchPage searchObject;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        // Before Testcase execution
+    }
+
+    @Test
+    public void login_with_valid_user(){
+        String username, password;
+        loginObject = new LoginPage();
+        searchObject = new SearchPage();
+        username = TestData.UserLoginData.validUser.getUserName();
+        password = TestData.UserLoginData.validUser.getPassword();
+        loginObject.login(username,password);
+        searchObject.getTextSearch().check(matches(isDisplayed()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //After Testcase execution
+    }
+}
+
+
+
+
+
---
+++ app/src/androidTest/java/com/mytaxi/android_demo/test/SearchActivityTest.java
@@ -0,0 +1,125 @@
+package com.mytaxi.android_demo.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnitRunner;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.mytaxi.android_demo.App;
+import com.mytaxi.android_demo.R;
+import com.mytaxi.android_demo.TestsApp;
+import com.mytaxi.android_demo.activities.AuthenticationActivity;
+import com.mytaxi.android_demo.activities.DriverProfileActivity;
+import com.mytaxi.android_demo.activities.MainActivity;
+import com.mytaxi.android_demo.data.TestData;
+import com.mytaxi.android_demo.dependencies.component.AppComponent;
+import com.mytaxi.android_demo.models.Driver;
+import com.mytaxi.android_demo.models.User;
+import com.mytaxi.android_demo.pages.DriverPage;
+import com.mytaxi.android_demo.pages.LoginPage;
+import com.mytaxi.android_demo.pages.SearchPage;
+import com.mytaxi.android_demo.utils.network.HttpClient;
+import com.mytaxi.android_demo.utils.storage.SharedPrefStorage;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Date;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.times;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.v4.content.ContextCompat.startActivity;
+import static org.hamcrest.Matchers.allOf;
+
+import javax.inject.Inject;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SearchActivityTest extends AndroidJUnitRunner {
+
+    LoginPage loginObject;
+    SearchPage searchObject;
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule(MainActivity.class,true,false);
+
+
+    String PACKAGE_ANDROID_DIALER = "com.android.dialer";
+    SearchPage searchPage = new SearchPage();
+    DriverPage driverPage = new DriverPage();
+
+    @Before
+    public void setUp(){
+        Intents.init();
+        // AuthenticationActivity.createIntent(MainActivity, AuthenticationActivity.class);
+        mActivityRule.launchActivity(new Intent());
+
+       // intended(hasComponent(MainActivity.class.getName()));
+
+        Intents.release();
+
+      // User user =  mSharedPrefStorage.loadUser();
+       // mActivityRule.launchActivity(null);
+       // dActivityRule.getActivity();
+        //       // .getSupportFragmentManager().beginTransaction();
+            //Intent intent = new Intent();
+           /* Driver driver=new Driver(TestData.DriverProfile.SarahDriver.getDriverName(),TestData.DriverProfile.SarahDriver.getDriverPhoneNum(),
+                    "ddd",TestData.DriverProfile.SarahDriver.getDriverLocation(), new Date()); */
+            //intent.putExtra("mainactivity",user);
+            //mActivityRule.launchActivity(intent);
+
+        //AppComponent appComponent = App.getApplicationContext(dActivityRule.getClass().).getAppComponent();
+        //appComponent.inject(dActivityRule.launchActivity(intent));
+
+        // to get the intent that started my activity
+      //  Bundle bundle = getIntent().getExtras();
+
+    }
+
+    @Test
+    public void testDriverProfileData(){
+        searchPage.SearchForExistingDriver(TestData.SearchData.SearchCriteria.getSearchData(),TestData.SearchData.SearchCriteria.getFullName());
+        driverPage.getDriverName().check(matches(withText(TestData.DriverProfile.SarahDriver.getDriverName())));
+        driverPage.getDriverLocation().check(matches(withText(TestData.DriverProfile.SarahDriver.getDriverLocation())));
+        driverPage.getDriverDate().check(matches(withText(TestData.DriverProfile.SarahDriver.getDriverDate())));
+        driverPage.getDriverPhoto().check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testToCallDriver(){
+        searchPage.SearchForExistingDriver(TestData.SearchData.SearchCriteria.getSearchData(),TestData.SearchData.SearchCriteria.getFullName());
+        driverPage.callDriver();
+        intended(allOf(
+                hasAction(Intent.ACTION_DIAL),
+                hasData(TestData.DriverProfile.SarahDriver.getDriverPhoneNum()),
+                toPackage(PACKAGE_ANDROID_DIALER)));
+    }
+
+
+    @Test
+    public void testSearchForDriver(){
+        String username, password;
+        loginObject = new LoginPage();
+        searchObject = new SearchPage();
+        username = TestData.UserLoginData.validUser.getUserName();
+        password = TestData.UserLoginData.validUser.getPassword();
+        loginObject.login(username,password);
+        searchObject.getTextSearch().check(matches(isDisplayed()));
+    }
+
+}
